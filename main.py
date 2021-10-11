68014d310949d6717cd571a9fdcdd2fcede7f7ed
evilyash <evilyash02@gmail.com>
2021-10-11 18:17:22 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import scipy
+import pandas as pd
+
+class KNN:
+    def __init__(self):
+        self.data = []
+
+
+
+
+def main():
+    dataset = pd.read_csv
